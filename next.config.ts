d47b9290037ec276b4fa4294206e0b292247425e
Gaspar Pérez Mazás <gaspypm@gmail.com>
2026-03-04 09:59:51 -0300

    Add necessary config files

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: "edge",
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
   },
 };
 
--- open-next.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "open-next";
-import cloudflare from "@opennextjs/cloudflare";
-
-export default defineConfig({
-    adapters: [cloudflare()],
-});
